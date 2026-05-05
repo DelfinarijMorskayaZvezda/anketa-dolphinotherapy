ad3ef023088632d95c77a0eac9c4cf3287f47baa
Kordeliyaa <JustAnya16@yandex.ru>
2026-05-05 16:45:30 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Questionnaire />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Questionnaire from './components/AnketaHealth.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
