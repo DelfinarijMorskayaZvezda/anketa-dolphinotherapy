bfd3ceb7babc76d79dfa90a6c0dff9b956f19a0c
Kordeliyaa <JustAnya16@yandex.ru>
2026-05-08 18:05:51 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/anketa-dolphinotherapy/' : './',
+  base: './',
+
+  //base: process.env.NODE_ENV === 'production' ? '/anketa-dolphinotherapy/' : './',
 })
