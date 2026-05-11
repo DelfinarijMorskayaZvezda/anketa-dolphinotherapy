439ba551cd9f1902e48e5baabd20910ff495aafa
Kordeliyaa <JustAnya16@yandex.ru>
2026-05-11 14:32:11 +0300

    Initial commit

--- src/components/AnketaHealth.vue
+++ src/components/AnketaHealth.vue
@@ -206,6 +206,8 @@ import { init, send } from '@emailjs/browser'
 import { ref, onMounted, computed } from 'vue'
 import ConsentSection from './ConsentSection.vue'
 
+const EMAILJS_PUBLIC_KEY = 'ZgwC2bywNLoZsnynT'
+
 const customAnswers = ref<{
   [key: number]: string
 }>({})
@@ -276,7 +278,7 @@ const isAnswerSelected = (questionId: number, answer: string): boolean => {
 
 // Загрузка сохранённых ответов и темы при монтировании компонента
 onMounted(() => {
-  init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
+  init(EMAILJS_PUBLIC_KEY)
 
   // Инициализация ответов
   selectedAnswers.value = {}
